e2c5164fecb137b9dc0132fcb1e0848d7e0bdf9e
Tom brady <tbrady@convoy.com>
2020-03-07 20:31:33 -0800

    add cors

--- server/src/register/middleware.registration.ts
+++ server/src/register/middleware.registration.ts
@@ -1,12 +1,23 @@
 import * as bodyParser from "body-parser";
 import * as express from "express";
 import * as swaggerUi from "swagger-ui-express";
+import cors from "cors";
+import * as env from "../environments/environment";
 
 const jsConfig = require('../swagger.json');
+
+const allowList: string[] = [
+    'http://localhost:4200',
+    env.allowedCors,
+];
+
+const corsOptions: cors.CorsOptions = {
+    origin: allowList
+};
 
 export function RegisterMiddleware(app: express.Express) {
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
-
+    app.use(cors(corsOptions));
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(jsConfig));
 }
